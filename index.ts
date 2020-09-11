0d4b187ee5c87c2dc5e4255d893d22e49eb5d0dd
Anwesh43 <anweshthecool0@gmail.com>
2020-09-11 15:02:03 +0530

    addin DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight
-const parts : number = 4  
+const parts : number = 5
 const scGap : number = 0.02 / parts 
 const delay : number = 20
 const strokeFactor : number = 90 
@@ -26,5 +26,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRotFromTopToCenter(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const y : number = -(h / 2 - size) * (1 - sf2)
+        const x : number = -0.4 * w + (-size + 0.4 * w) * sf4 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(x, y)
+            context.rotate(Math.PI / 2 * sf3)
+            DrawingUtil.drawLine(context, 0, -size, 0, -size + size * sf1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawRFTCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRotFromTopToCenter(context, scale)
     }
 }
